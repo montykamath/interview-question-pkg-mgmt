920771b4c0454cae110b8f1e6e952eb97fd09de7
Monty Kamath <monty@goodstart.com>
2015-08-09 18:46:21 -0400

    fixed tiny eclipse java compiler warning

--- test/com/goodstart/programmingtest/PackageManagerTest.java
+++ test/com/goodstart/programmingtest/PackageManagerTest.java
@@ -1,11 +1,12 @@
 package com.goodstart.programmingtest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
